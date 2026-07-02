d5bb5e77dfa2a28f97730d9a09d0768d81e9b4af
Devendra234 <devendramarathe77@gmail.com>
2026-07-03 00:36:04 +0530

    Implement authentication foundation

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 from fastapi import FastAPI
-
 from app.database.database import Base, engine
+from app.auth.security import hash_password
 
 app = FastAPI(title="Employee Management API")
 
 Base.metadata.create_all(bind=engine)
 
-
 @app.get("/")
 def home():
     return {"message": "Employee Management API Running"}
+
+@app.get("/test")
+def test():
+    return {
+        "password": hash_password("admin123")
+        
+    }
